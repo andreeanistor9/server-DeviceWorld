6219bd01a43d8dc634a7899315219e9076d0ee62
Nistor <andreea.nistor@dell.com>
2023-04-19 17:28:00 +0300

    bd

--- server.js
+++ server.js
@@ -1,39 +1,62 @@
-const express = require('express')
-const app = express()
-const {Client} = require("pg")
-const session = require('express-session')
-
-var client = new Client({user:"andreean", password:"parola", host:"localhost", port:5432, database:"db_electronix"});
-protocol="http://";
-domainName="localhost:5000";
-client.connect((err)=>{
-    if(err) throw err;
-    console.log("Connected");
+const express = require("express");
+const { Client } = require("pg");
+const session = require("express-session");
+const formidable = require("formidable");
+const app = express();
+protocol = "http://";
+domainName = "localhost:5000";
+var client = new Client({
+  user: "andreean",
+  password: "parola",
+  host: "localhost",
+  port: 5432,
+  database: "db_electronix",
 });
 
-app.use(session({
-    secret:'abcdefg',
-    resave: true,
-    saveUninitialized: false
-}));
-app.use("/*", function(req, res, next){
-    res.locals.user = req.session.user;
-    next();
-})
-// app.get("/products", (req, res) =>{
-//    // if(req.session && req.session.user && req.session.user.role == "admin"){
-//         client.query("select * from users", function(err, result) {
-//             if(err) throw err
-//             console.log(result)
-//            res.render("/Pages/Products", {products:result.rows})
-//        })
-//     // }
-//     // else {
-//     //     restart.status(403).render(`You don't have access`);
-//     // }
-// })
-app.get("/products",(req, res) =>{
-    res.json({"products": ["product1", "product2", "product3", "product5"]})
-})
+client.connect((err) => {
+  if (err) throw err;
+  console.log("Connected");
+});
 
-app.listen(5000, () => {console.log("Server started on port 5000")})
+app.use(
+  session({
+    secret: "abcdefg",
+    resave: true,
+    saveUninitialized: false,
+  })
+);
+//get all products from database
+app.get("/products", async (req, res) => {
+  try {
+    const allProducts = await client.query(`select * from products`);
+    res.json(allProducts.rows);
+    // console.log(allProducts.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+// app.get("/product", async (req, res) => {
+//   try {
+//     const product = await client.query(
+//       `select * from products where id=${req.params}`
+//     );
+//     res.json(product.rows);
+//     console.log(product.rows);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+//get all users from database
+app.get("/users", async (req, res) => {
+  try {
+    const allUsers = await client.query(`select * from users`);
+    res.json(allUsers.rows);
+    console.log(allUsers.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
